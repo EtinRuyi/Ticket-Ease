7ff3644a5715c93f6ead26a6b4b1e6e9a13a9666
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-11-04 14:10:02 +0100

    Pagination and Swagger Configuratiom-be

--- Ticket-Ease/Ticket-Ease/Program.cs
+++ Ticket-Ease/Ticket-Ease/Program.cs
@@ -1,3 +1,5 @@
+using Ticket_Ease.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ticket Ease v1"));
 }
 
 app.UseHttpsRedirection();
