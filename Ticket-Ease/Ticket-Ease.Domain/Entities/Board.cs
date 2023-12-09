d7dd7a51619adaad3d6377593556f7517db71852
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-09 19:05:10 +0100

    Updated Ticket-Ease

--- Ticket-Ease/Ticket-Ease.Domain/Entities/Board.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TicketEase.Domain.Entities
-{
-    public class Board : BaseEntity
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public ICollection<Project> Projects { get; set; }
-    }
-}
