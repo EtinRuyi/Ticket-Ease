515cdc4499694855805d70762711398ca094417a
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-13 08:53:41 +0100

    UpdatedIdentityService

--- TicketEase/Configurations/IdentitySeviceExtension.cs
+++ TicketEase/Configurations/IdentitySeviceExtension.cs
@@ -4,28 +4,23 @@ using TicketEase.Persistence.Context;
 
 namespace TicketEase.Configurations
 {
-    public static class IdentityServiceExtension
-    {
-        public static void IdentityConfiguration(this IServiceCollection services)
-        {
-            var builder = services.AddIdentity<AppUser, IdentityRole>(options =>
-            {
-                options.User.RequireUniqueEmail = true;
-                options.SignIn.RequireConfirmedEmail = false;
-                options.Password.RequireDigit = false; //return this to true
-                options.Password.RequireNonAlphanumeric = false; //return this to true
-				options.Password.RequireUppercase = false; //return this to true
+	public static class IdentityServiceExtension
+	{
+		public static void IdentityConfiguration(this IServiceCollection services)
+		{
+			var builder = services.AddIdentity<AppUser, IdentityRole>(options =>
+			{
+				options.User.RequireUniqueEmail = true;
+				options.SignIn.RequireConfirmedEmail = false;
+				options.Password.RequireDigit = false; 
+				options.Password.RequireNonAlphanumeric = false; 
+				options.Password.RequireUppercase = false; 
 				options.Password.RequireLowercase = true;
-                options.Password.RequiredLength = 6;
-            });
-           // builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
-            builder.AddEntityFrameworkStores<TicketEaseDbContext>().AddDefaultTokenProviders();
+				options.Password.RequiredLength = 6;
+			});
+		   
+			builder.AddEntityFrameworkStores<TicketEaseDbContext>().AddDefaultTokenProviders();
 
-
-            //services.AddIdentity<Manager, IdentityRole>()
-            //	.AddEntityFrameworkStores<TicketEaseDbContext>()
-            //	.AddDefaultTokenProviders();
-
-        }
+		}
 	}
 }
